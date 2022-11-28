d7c292b71ff58247bc676fda3df16819ecac820a
Tiago Matana <tiago@withleaf.io>
2022-11-28 12:14:16 -0300

    app funcinal

--- app/src/main/java/com/edina/compras/InicioActivity.java
+++ app/src/main/java/com/edina/compras/InicioActivity.java
@@ -23,7 +23,7 @@ import com.edina.compras.model.User;
 import java.util.ArrayList;
 import java.util.concurrent.Callable;
 
-public class InicioActivity extends AppCompatActivity {
+public class InicioActivity extends AppCompatActivity implements Callback {
 
     private RecyclerView recyclerView;
     private ItemDAO itemDAO;
@@ -35,14 +35,11 @@ public class InicioActivity extends AppCompatActivity {
         itemDAO = new ItemDAO(this);
         ArrayList<Item> items = itemDAO.load(logado);
 
-        ItemAdapter adapter = new ItemAdapter(items);
+        ItemAdapter adapter = new ItemAdapter(items, this);
         recyclerView.setAdapter(adapter);
         RecyclerView.LayoutManager layout = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layout);
     }
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,66 +50,83 @@ public class InicioActivity extends AppCompatActivity {
         if (!logado.isLogged()) {
             startActivity(new Intent(this, MainActivity.class));
         }
-        getSupportActionBar().setTitle("Bem-vindo " + logado.getNome().toUpperCase());
+        getSupportActionBar().setTitle("Bem-vindo");
         lista(logado);
 
-        // Adicionar item
         findViewById(R.id.btAddItem).setOnClickListener(view -> {
-            LayoutInflater li = getLayoutInflater();
-            View v = li.inflate(R.layout.activity_gerenciar_item, null);
-            v.findViewById(R.id.btAdicionar).setOnClickListener(viewDialog -> {
-                EditText descricao = v.findViewById(R.id.addDescricaoText);
-                EditText quantidade = v.findViewById(R.id.addQuantidadeText);
-                Item item = new Item();
-                item.setEmail(logado.getEmail());
-                item.setQuantidade(Integer.parseInt(quantidade.getText().toString()));
-                item.setDescricao(descricao.getText().toString());
-                Long id = itemDAO.save(item);
-                Toast.makeText(this, "Adicionado com sucesso: ID " + id.toString(), Toast.LENGTH_SHORT).show();
-                lista(logado);
-                adicionarItem.dismiss();
-            });
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Adicionar Item");
-            builder.setCancelable(Boolean.TRUE);
-            builder.setView(v);
-            adicionarItem = builder.create();
-            adicionarItem.show();
+            this.adicionarItem(logado);
         });
-
-
     }
 
-    public void editarItem(Item item) {
-        // Editar item
-
+    public void adicionarItem(User logado) {
         LayoutInflater li = getLayoutInflater();
         View v = li.inflate(R.layout.activity_gerenciar_item, null);
-        Button editarButton = v.findViewById(R.id.btAdicionar);
-        editarButton.setText("Alterar");
         EditText descricao = v.findViewById(R.id.addDescricaoText);
         EditText quantidade = v.findViewById(R.id.addQuantidadeText);
-        descricao.setText(item.getDescricao());
-        quantidade.setText(item.getQuantidade());
-        editarButton.setOnClickListener(viewDialog -> {
-            item.setQuantidade(Integer.parseInt(quantidade.getText().toString()));
-            item.setDescricao(descricao.getText().toString());
-
-
-            itemDAO.update(item);
-            Toast.makeText(this, "Alterado com sucesso.", Toast.LENGTH_SHORT).show();
-            adicionarItem.dismiss();
-        });
+        Item item = new Item();
+        item.setEmail(logado.getEmail());
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Adicionar Item");
         builder.setCancelable(Boolean.TRUE);
         builder.setView(v);
+        builder.setPositiveButton("Salvar", (dialogInterface, i) -> {
+            item.setQuantidade(Integer.parseInt(quantidade.getText().toString()));
+            item.setDescricao(descricao.getText().toString());
+            itemDAO.save(item);
+            Toast.makeText(this, "Alterado com sucesso.", Toast.LENGTH_SHORT).show();
+            logado.setEmail(item.getEmail());
+            lista(logado);
+        });
+        builder.setNegativeButton("Cancelar", (dialogInterface, i) -> {
+            lista(logado);
+        });
         adicionarItem = builder.create();
         adicionarItem.show();
-
     }
 
+    public void editarItem(Item item) {
+        LayoutInflater li = getLayoutInflater();
+        View v = li.inflate(R.layout.activity_gerenciar_item, null);
+        EditText descricao = v.findViewById(R.id.addDescricaoText);
+        EditText quantidade = v.findViewById(R.id.addQuantidadeText);
+        descricao.setText(item.getDescricao());
+        quantidade.setText(Integer.toString(item.getQuantidade()));
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Adicionar Item");
+        User logado = new User();
+        builder.setPositiveButton("Atualizar", (dialogInterface, i) -> {
+            item.setQuantidade(Integer.parseInt(quantidade.getText().toString()));
+            item.setDescricao(descricao.getText().toString());
+            itemDAO.update(item);
+            Toast.makeText(this, "Alterado com sucesso.", Toast.LENGTH_SHORT).show();
+            logado.setEmail(item.getEmail());
+            lista(logado);
+        });
+        builder.setNegativeButton("Cancelar", (dialogInterface, i) -> {
 
+        });
+        builder.setView(v);
+        adicionarItem = builder.create();
+        adicionarItem.show();
+    }
+
+    public void removerItem(Item item) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("REMOVER ITEM");
+        builder.setMessage("Tem certeza que deseja remover o item " + item.getDescricao() +"?");
+        User logado = new User();
+        builder.setPositiveButton("Remover", (dialogInterface, i) -> {
+            itemDAO.remove(item);
+            Toast.makeText(this, "Removido com sucesso.", Toast.LENGTH_SHORT).show();
+            logado.setEmail(item.getEmail());
+            lista(logado);
+        });
+        builder.setNegativeButton("Cancelar", (dialogInterface, i) -> {
+
+        });
+        adicionarItem = builder.create();
+        adicionarItem.show();
+    }
 
 
     public boolean onOptionsItemSelected(MenuItem item){
